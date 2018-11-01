fcab95789cd8487160d51e7908d0288e3f9209d1
Marlon Braga <rdg.brg@gmail.com>
2018-11-01 19:39:46 -0400

    API added

--- app/controllers/first_stages_controller.rb
+++ app/controllers/first_stages_controller.rb
@@ -1,19 +1,19 @@
 class FirstStagesController < ApplicationController
   def index
-  #   countries = ["US","BR","FR","GB","NZ","AU", "SCT", "MX", "SP", "AR", "CA", "IT"];
+    countries = ["US","BR","FR","GB","NZ","AU", "SCT", "MX", "SP", "AR", "CA", "IT"];
 
-  #   response = HTTParty.get('https://randomuser.me/api/?results=5')
-  #     data = response.parsed_response["results"]
+    response = HTTParty.get('https://randomuser.me/api/?results=5')
+      data = response.parsed_response["results"]
       
-  #   for i in 0...data.length do 
-  #       if(countries.include?(data[i]['nat']))
-  #         SaleRep.create(
-  #           first_name: data[i]['name']['first'],
-  #           last_name:  data[i]['name']['last'],
-  #           photo_url:  data[i]['picture']['large']
-  #         )
-  #       end
-  #    end
+    for i in 0...data.length do 
+        if(countries.include?(data[i]['nat']))
+          SaleRep.create(
+            first_name: data[i]['name']['first'],
+            last_name:  data[i]['name']['last'],
+            photo_url:  data[i]['picture']['large']
+          )
+        end
+     end
 
     @sale_rep = SaleRep.all
 
